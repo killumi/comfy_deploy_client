28c38f929784f103347e06a70a98772d28e7c808
Maxim Panasenko <fanalis@MacBook-Pro-Maxim.local>
2025-05-27 12:40:55 +0300

    comfy_deploy_client

---
+++ lib/types_extensions.dart
@@ -0,0 +1,13 @@
+import 'package:comfy_deploy_client/types.dart';
+
+extension TypesExtensions on RunOutputResult {
+  String? findOutputImageUrl() {
+    for (var output in outputs) {
+      if (output.data.containsKey('images')) {
+        final image = output.data['images'].first['url'];
+        return image;
+      }
+    }
+    return null;
+  }
+}
