33ec5289a5be5a5f0b5588414f244e07361dafd4
Maxim Panasenko <fanalis@MacBook-Pro-Maxim.local>
2025-06-10 02:13:41 +0300

    add multiplatform comfy clients

---
+++ lib/v2_web/comfy_deploy_platform_client.dart
@@ -0,0 +1,90 @@
+library comfy_deploy_client;
+
+import 'package:comfy_deploy_client/types.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:retrofit/retrofit.dart';
+part 'comfy_deploy_platform_client.g.dart';
+
+@RestApi()
+abstract class ComfyDeployPlatformClient {
+  factory ComfyDeployPlatformClient({
+    required String apiKey,
+    Dio? dio,
+    String? proxyUrl,
+  }) {
+    final clientDio = dio ?? Dio();
+    clientDio.options.headers['authorization'] = 'Bearer $apiKey';
+
+    const baseApiUrl = 'https://api.comfydeploy.com/api';
+
+    final baseUrl = kIsWeb && proxyUrl != null && proxyUrl.isNotEmpty
+        ? '$proxyUrl?proxy_url=${Uri.encodeComponent(baseApiUrl)}'
+        : baseApiUrl;
+
+    return _ComfyDeployPlatformClient(clientDio, baseUrl: baseUrl);
+  }
+
+  @GET('/upload-url')
+  Future<UploadUrlResult> getUploadUrl({
+    @Query('type') String? type,
+    @Query('file_size') int? fileSize,
+  });
+
+  @POST('/run/deployment/queue')
+  Future<RunResult> postRun({
+    @Body() required RunData data,
+  });
+
+  @GET('/run/{runId}')
+  Future<RunOutputResult> getRun({
+    @Path('runId') required String runId,
+  });
+
+  @GET('/websocket/{deploymentId}')
+  Future<WebsocketResult> getWebsocket({
+    @Path() required String deploymentId,
+  });
+}
+
+// ------------------------------
+
+@RestApi()
+abstract class UploadImagePlatformClient {
+  factory UploadImagePlatformClient({
+    required String token,
+    Dio? dio,
+    bool enableLogging = true,
+    String? proxyUrl,
+  }) {
+    final clientDio = dio ?? Dio();
+
+    final authToken = token.startsWith('Bearer ') ? token : 'Bearer $token';
+    clientDio.options.headers['X-Authorization'] = authToken;
+
+    if (enableLogging) {
+      clientDio.interceptors.add(
+        LogInterceptor(
+          responseBody: true,
+          requestBody: true,
+          error: true,
+          logPrint: (o) => print(o),
+        ),
+      );
+    }
+
+    const baseApiUrl = 'http://159.223.239.38:8080/comfy';
+
+    final baseUrl = kIsWeb && proxyUrl != null && proxyUrl.isNotEmpty
+        ? '$proxyUrl?proxy_url=${Uri.encodeComponent(baseApiUrl)}'
+        : baseApiUrl;
+
+    return _UploadImagePlatformClient(clientDio, baseUrl: baseUrl);
+  }
+
+  @GET('/upload-url')
+  Future<UploadUrlResult> getImageUploadUrl({
+    @Query('type') String? type,
+    @Query('file_size') int? fileSize,
+  });
+}
---
+++ lib/v2_web/comfy_deploy_platform_client.g.dart
@@ -0,0 +1,279 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'comfy_deploy_platform_client.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+
+class _ComfyDeployPlatformClient implements ComfyDeployPlatformClient {
+  _ComfyDeployPlatformClient(
+    this._dio, {
+    this.baseUrl,
+    this.errorLogger,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<UploadUrlResult> getUploadUrl({
+    String? type,
+    int? fileSize,
+  }) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'type': type,
+      r'file_size': fileSize,
+    };
+    queryParameters.removeWhere((k, v) => v == null);
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<UploadUrlResult>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/upload-url',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UploadUrlResult _value;
+    try {
+      _value = UploadUrlResult.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<RunResult> postRun({required RunData data}) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(data.toJson());
+    final _options = _setStreamType<RunResult>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/run/deployment/queue',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late RunResult _value;
+    try {
+      _value = RunResult.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<RunOutputResult> getRun({required String runId}) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<RunOutputResult>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/run/${runId}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late RunOutputResult _value;
+    try {
+      _value = RunOutputResult.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<WebsocketResult> getWebsocket({required String deploymentId}) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<WebsocketResult>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/websocket/${deploymentId}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late WebsocketResult _value;
+    try {
+      _value = WebsocketResult.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+
+class _UploadImagePlatformClient implements UploadImagePlatformClient {
+  _UploadImagePlatformClient(
+    this._dio, {
+    this.baseUrl,
+    this.errorLogger,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<UploadUrlResult> getImageUploadUrl({
+    String? type,
+    int? fileSize,
+  }) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'type': type,
+      r'file_size': fileSize,
+    };
+    queryParameters.removeWhere((k, v) => v == null);
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<UploadUrlResult>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/upload-url',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UploadUrlResult _value;
+    try {
+      _value = UploadUrlResult.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
